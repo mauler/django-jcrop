35a13b450f798e34e20ba64bac0cc456305d1022
Mathieu Comandon <strycore@gmail.com>
2012-08-17 12:37:59 +0200

    Added packaging and legal stuff

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='django-jcrop',
+    version='0.1',
+    description='Django app providing cropping functionnality with jcrop',
+    author='Mathieu Comandon',
+    author_email='strider@strycore.com',
+    url='https://github.com/strycore/django-jcrop',
+    packages=['django_crop'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities'
+        'Environment :: Web Environment',
+        'Framework :: Django',
+    ],
+)

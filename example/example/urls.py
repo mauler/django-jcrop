ca99d467f08640a6283635a0d3bc6294e8b068e8
Mathieu Comandon <strycore@gmail.com>
2013-02-11 17:24:37 +0100

    Redirect base URL to admin page

--- example/example/urls.py
+++ example/example/urls.py
@@ -1,16 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # url(r'^$', 'example.views.home', name='home'),
+urlpatterns = patterns(
+    '',
+    url(r'^$', RedirectView.as_view(url='/admin/')),
     url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
-    urlpatterns += patterns('',
+    urlpatterns += patterns(
+        '',
         (r'^media/(?P<path>.*)$', 'django.views.static.serve',
          {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
     )

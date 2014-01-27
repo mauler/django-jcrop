7b9048d6b92454f338960d5804fe84469ac17fd5
Paulo R <proberto.macedo@gmail.com>
2014-01-27 12:08:25 -0200

    Increments version

--- django_jcrop/models.py
+++ django_jcrop/models.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-"""Definition of JCropImageField with associated widget"""
-
-from django import forms
 from django.db import models
-from django_jcrop.forms import JCropAdminImageWidget
+from django import forms
+
+from .forms import JCropAdminImageWidget
+
 
 class JCropImageField(models.ImageField):
 
     def formfield(self, **kwargs):
         defaults = kwargs
-        defaults.update({'form_class': forms.ImageField,
-                         'widget': JCropAdminImageWidget})
+        defaults.update({
+            'form_class': forms.ImageField,
+             'widget': JCropAdminImageWidget
+        })
         return super(JCropImageField, self).formfield(**defaults)
